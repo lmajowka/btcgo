e907868af2f24ae15a0178b190046a91cbb52a8f
Leo <leo.majowka@gmail.com>
2024-06-29 21:52:45 +0300

    Bump version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BTC GO v0.3
+# BTC GO v0.4
 
 # Instruções para rodar o projeto (na sua máquina)
 
--- src/main.go
+++ src/main.go
@@ -49,21 +49,20 @@ func main() {
 	}
 
 	color.Cyan("BTC GO - Investidor Internacional")
-	color.Yellow("v0.3")
+	color.Yellow("v0.4")
 
 	// Ask the user for the range number
 	rangeNumber := PromptRangeNumber(len(ranges.Ranges))
 
-
 	// Pergunta sobre modos de usar
 	modoSelecionado := PromptModos(2) // quantidade de modos
 
-    var carteirasalva string
-    carteirasalva = fmt.Sprintf("%d", rangeNumber)
-    privKeyInt := new(big.Int)
+	var carteirasalva string
+	carteirasalva = fmt.Sprintf("%d", rangeNumber)
+	privKeyInt := new(big.Int)
 
-    // função HandleModoSelecionado - onde busca o modo selecionado do usuario. // talvez criar a funcao de favoritar essa opções e iniciar automaticamente?
-    privKeyInt = HandleModoSelecionado(modoSelecionado, ranges, rangeNumber, privKeyInt, carteirasalva)
+	// função HandleModoSelecionado - onde busca o modo selecionado do usuario. // talvez criar a funcao de favoritar essa opções e iniciar automaticamente?
+	privKeyInt = HandleModoSelecionado(modoSelecionado, ranges, rangeNumber, privKeyInt, carteirasalva)
 
 	// Load wallet addresses from JSON file
 	wallets, err := LoadWallets(filepath.Join(rootDir, "data", "wallets.json"))
@@ -89,14 +88,11 @@ func main() {
 	// Create a wait group to wait for all workers to finish
 	var wg sync.WaitGroup
 
-
-
 	// Start worker goroutines
 	for i := 0; i < cpusNumber; i++ {
 		wg.Add(1)
 		go worker(wallets, privKeyChan, resultChan, &wg)
 	}
-
 
 	// Ticker for periodic updates every 5 seconds
 	ticker := time.NewTicker(5 * time.Second)
@@ -107,16 +103,15 @@ func main() {
 	var lastkey string
 	// Goroutine to update last processed wallet address
 	go func() {
-	for {
-	    select {
-	    case privKey := <-privKeyChan:
-	        lastkey = fmt.Sprintf("%064x", privKey)
-	    case <-done:
-	        return
-	    }
-	}
+		for {
+			select {
+			case privKey := <-privKeyChan:
+				lastkey = fmt.Sprintf("%064x", privKey)
+			case <-done:
+				return
+			}
+		}
 	}()
-
 
 	// Goroutine to print speed updates
 	go func() {
@@ -126,7 +121,9 @@ func main() {
 				elapsedTime := time.Since(startTime).Seconds()
 				keysPerSecond := float64(keysChecked) / elapsedTime
 				fmt.Printf("Chaves checadas: %s Chaves por segundo: %s\n", humanize.Comma(int64(keysChecked)), humanize.Comma(int64(keysPerSecond)))
-				if(modoSelecionado == 2){saveUltimaKeyWallet("ultimaChavePorCarteira.txt", carteirasalva, lastkey)}			
+				if modoSelecionado == 2 {
+					saveUltimaKeyWallet("ultimaChavePorCarteira.txt", carteirasalva, lastkey)
+				}
 			case <-done:
 				return
 			}
@@ -174,12 +171,12 @@ func main() {
 			file.Close()
 		}
 
-	if(modoSelecionado == 2){
-		foundAddressString = fmt.Sprintf("%064x", foundAddress)
-		saveUltimaKeyWallet("ultimaChavePorCarteira.txt", carteirasalva, foundAddressString)
-	}			
+		if modoSelecionado == 2 {
+			foundAddressString = fmt.Sprintf("%064x", foundAddress)
+			saveUltimaKeyWallet("ultimaChavePorCarteira.txt", carteirasalva, foundAddressString)
+		}
 
-	close(privKeyChan)
+		close(privKeyChan)
 
 	}
 
@@ -193,7 +190,6 @@ func main() {
 	fmt.Printf("Chaves por segundo: %s\n", humanize.Comma(int64(keysPerSecond)))
 
 }
-
 
 // start na workers
 func worker(wallets *Wallets, privKeyChan <-chan *big.Int, resultChan chan<- *big.Int, wg *sync.WaitGroup) {
@@ -210,4 +206,3 @@ func worker(wallets *Wallets, privKeyChan <-chan *big.Int, resultChan chan<- *bi
 		}
 	}
 }
-
